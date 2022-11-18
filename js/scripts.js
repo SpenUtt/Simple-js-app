f50008a8a2532d655055d0f4cfe159ad02262b26
SpenUtt <spencerutt8@gmail.com>
2022-11-18 13:08:06 +0100

    Created list array with 5 Pokemon

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+//Array list
+
+let pokemonList=[
+    {
+        name: "Pikachu", 
+        type: ["electric"], 
+        height: 0.4
+    }, 
+    {
+        name: "Squirtle", 
+        type: ["water"], 
+        height: 0.5
+    },
+    {
+        name: "Charmander", 
+        type: ["fire"], 
+        height: 0.6
+    },
+    {
+        name: "Jigglypuff", 
+        type: ["fairy", "normal"], 
+        height: 0.5
+    },
+    {
+        name: "Slowbro",
+        type: ["psychic", "water"],
+        height: 1.6
+    }
+];
+
+//note heights given in meters 
