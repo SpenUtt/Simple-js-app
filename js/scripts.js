46c8cd0cf697da84ea821cd8d1bf055b92223a72
SpenUtt <spencerutt8@gmail.com>
2022-11-23 11:13:02 +0100

    changed double quotation marks to single

--- js/scripts.js
+++ js/scripts.js
@@ -2,33 +2,33 @@
 
 let pokemonList=[
     {
-        name: "Pikachu", 
-        types: ["electric"], 
+        name: 'Pikachu', 
+        types: ['electric'], 
         height: 0.4
     }, 
     {
-        name: "Squirtle", 
-        types: ["water"], 
+        name: 'Squirtle', 
+        types: ['water'], 
         height: 0.5
     },
     {
-        name: "Charmander", 
-        types: ["fire"], 
+        name: 'Charmander', 
+        types: ['fire'], 
         height: 0.6
     },
     {
-        name: "Jigglypuff", 
-        types: ["fairy", "normal"], 
+        name: 'Jigglypuff', 
+        types: ['fairy', 'normal'], 
         height: 0.5
     },
     {
-        name: "Slowbro",
-        types: ["psychic", "water"],
+        name: 'Slowbro',
+        types: ['psychic', 'water'],
         height: 1.6
     },
     {
-        name: "Breloom",
-        types: ["grass", "fighting"],
+        name: 'Breloom',
+        types: ['grass', 'fighting'],
         height: 1.2
     }
 ];
@@ -55,7 +55,7 @@ document.write('<p>Breloom (height 1.2) </p> ')
 for (let i = 0; 
     i < pokemonList.length; i++) {
     if(pokemonList[i].height >= 1.5) {
-            document.write(pokemonList[i].name + pokemonList[i].height + "Wow, that is a big pokemon! <br>");
+            document.write(pokemonList[i].name + ' ' + 'height:\ (' + pokemonList[i].height + '\) 'Wow, that is a big pokemon! <br>');
         }
         else if (pokemonList[i].height >= 0.5 && pokemonList[i].height < 1.5){
             document.write(pokemonList[i].name + pokemonList[i].height + "Wow, that is an average sized pokemon! <br>");
