cafe8c44377dcbf3e61e633d272c43bf5a121972
SpenUtt <spencerutt8@gmail.com>
2022-11-25 20:59:09 +0100

    replaced loop with forEach function

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,37 @@
-//Array list
-
 let pokemonList=[
-    {
-        name: 'Pikachu', 
-        types: ['electric'], 
-        height: 0.4
-    }, 
-    {
-        name: 'Squirtle', 
-        types: ['water'], 
-        height: 0.5
-    },
-    {
-        name: 'Charmander', 
-        types: ['fire'], 
-        height: 0.6
-    },
-    {
-        name: 'Jigglypuff', 
-        types: ['fairy', 'normal'], 
-        height: 0.5
-    },
-    {
-        name: 'Slowbro',
-        types: ['psychic', 'water'],
-        height: 1.6
-    },
-    {
-        name: 'Breloom',
-        types: ['grass', 'fighting'],
-        height: 1.2
-    }
+{
+    name: 'Pikachu', 
+    types: ['electric'], 
+    height: 0.4
+}, 
+{
+    name: 'Squirtle', 
+    types: ['water'], 
+    height: 0.5
+},
+{
+    name: 'Charmander', 
+    types: ['fire'], 
+    height: 0.6
+},
+{
+    name: 'Jigglypuff', 
+    types: ['fairy', 'normal'], 
+    height: 0.5
+},
+{
+    name: 'Slowbro',
+    types: ['psychic', 'water'],
+    height: 1.6
+},
+{
+    name: 'Breloom',
+    types: ['grass', 'fighting'],
+    height: 1.2
+}
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
+/*for (let i = 0; i < pokemonList.length; i++) {
     if(pokemonList[i].height >= 1.5) {
         document.write(pokemonList[i].name + ' ' + 'height:\(' + pokemonList[i].height + '\) ' + ' Wow, that is a big pokemon! <br>');
     }
@@ -43,4 +41,8 @@ for (let i = 0; i < pokemonList.length; i++) {
     else {
         document.write(pokemonList[i].name + ' ' + 'height:\(' + pokemonList[i].height + '\) ' + ' That is a small pokemon, how cute! <br>');
     }
-}
+}*/
+pokemonList.forEach(function(pokemon) {
+    document.write(pokemon.name + ' is a ' + pokemon.types + ' type pokemon and is ' + pokemon.height + ' meters tall. <br> ');
+})
+
