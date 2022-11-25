999424b42e9cccc5883378745fe95175dd65c1f2
SpenUtt <spencerutt8@gmail.com>
2022-11-25 21:11:13 +0100

    wrapped array in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,51 @@
-let pokemonList=[
-{
-    name: 'Pikachu', 
-    types: ['electric'], 
-    height: 0.4
-}, 
-{
-    name: 'Squirtle', 
-    types: ['water'], 
-    height: 0.5
-},
-{
-    name: 'Charmander', 
-    types: ['fire'], 
-    height: 0.6
-},
-{
-    name: 'Jigglypuff', 
-    types: ['fairy', 'normal'], 
-    height: 0.5
-},
-{
-    name: 'Slowbro',
-    types: ['psychic', 'water'],
-    height: 1.6
-},
-{
-    name: 'Breloom',
-    types: ['grass', 'fighting'],
-    height: 1.2
-}
-];
+(function () {
+    let data= {};
 
-/*for (let i = 0; i < pokemonList.length; i++) {
-    if(pokemonList[i].height >= 1.5) {
-        document.write(pokemonList[i].name + ' ' + 'height:\(' + pokemonList[i].height + '\) ' + ' Wow, that is a big pokemon! <br>');
+    let pokemonList=[
+    {
+        name: 'Pikachu', 
+        types: ['electric'], 
+        height: 0.4
+    }, 
+    {
+        name: 'Squirtle', 
+        types: ['water'], 
+        height: 0.5
+    },
+    {
+        name: 'Charmander', 
+        types: ['fire'], 
+        height: 0.6
+    },
+    {
+        name: 'Jigglypuff', 
+        types: ['fairy', 'normal'], 
+        height: 0.5
+    },
+    {
+        name: 'Slowbro',
+        types: ['psychic', 'water'],
+        height: 1.6
+    },
+    {
+        name: 'Breloom',
+        types: ['grass', 'fighting'],
+        height: 1.2
     }
-    else if (pokemonList[i].height >= 0.5 && pokemonList[i].height < 1.5){
-        document.write(pokemonList[i].name + ' ' + 'height:\(' + pokemonList[i].height + '\) ' + ' That pokemon is an average size. <br>');
-    }
-    else {
-        document.write(pokemonList[i].name + ' ' + 'height:\(' + pokemonList[i].height + '\) ' + ' That is a small pokemon, how cute! <br>');
-    }
-}*/
-pokemonList.forEach(function(pokemon) {
-    document.write(pokemon.name + ' is a ' + pokemon.types + ' type pokemon and is ' + pokemon.height + ' meters tall. <br> ');
-})
+    ];
 
+    /*for (let i = 0; i < pokemonList.length; i++) {
+        if(pokemonList[i].height >= 1.5) {
+            document.write(pokemonList[i].name + ' ' + 'height:\(' + pokemonList[i].height + '\) ' + ' Wow, that is a big pokemon! <br>');
+        }
+        else if (pokemonList[i].height >= 0.5 && pokemonList[i].height < 1.5){
+            document.write(pokemonList[i].name + ' ' + 'height:\(' + pokemonList[i].height + '\) ' + ' That pokemon is an average size. <br>');
+        }
+        else {
+            document.write(pokemonList[i].name + ' ' + 'height:\(' + pokemonList[i].height + '\) ' + ' That is a small pokemon, how cute! <br>');
+        }
+    }*/
+    pokemonList.forEach(function(pokemon) {
+        document.write(pokemon.name + ' is a ' + pokemon.types + ' type pokemon and is ' + pokemon.height + ' meters tall. <br> ');
+    })
+})();
