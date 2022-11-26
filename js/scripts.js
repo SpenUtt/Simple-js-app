f1116bd311c2236d029f2a3a43d1e8f2b776e29c
SpenUtt <spencerutt8@gmail.com>
2022-11-26 18:32:12 +0100

    edits to repository getAll function

--- js/scripts.js
+++ js/scripts.js
@@ -44,18 +44,15 @@ let pokemonRepository = (function () {
     return {
         getAll: getAll,
         add: add,
-    };
-
-    pokemonRepository.getAll().forEach(function(pokemon){
-        if(pokemon.height > 1.5){
-            document.write(<li>pokemon.name + ' (height: '+ pokemon.height + ') - Wow, this is a large Pokemon!' + '</li>')
-        }
-        else {
-            document.write(<li>pokemon.name + ' (height: '+ pokemon.height + ') </li>')
-        }
-    }) 
+    };   
 })()
-/*
-pokemonList.forEach(function(pokemon) {
-    document.write(pokemon.name + ' is a ' + <li>pokemon.types + ' type pokemon and is ' + pokemon.height + ' meters tall. '</li>);
-})*/
+
+pokemonRepository.getAll().forEach(function(pokemon){
+    if(pokemon.height > 1.5){
+        document.write(pokemon.name + ' (height: '+ pokemon.height + ') - Wow, this is a large Pokemon!' + '<br>')
+    }
+    else {
+        document.write(pokemon.name + ' (height: '+ pokemon.height + ') <br>');
+    }
+})
+
